894cdd9d92fe05eb7c51380e8e41d5ea2ec944e2
Jan <jan.caspar@gmail.com>
2022-11-12 21:14:29 +0100

    Updates

---
+++ assets/OidcCallback.2282e332.js
@@ -0,0 +1,1 @@
+import{S as t,i as e,s as n,aa as l}from"./index.e5f15ed8.js";function c(a){return l(),[]}class i extends t{constructor(s){super(),e(this,s,c,null,n,{})}}export{i as default};
---
+++ assets/OidcSilentRenew.59ef3c59.js
@@ -0,0 +1,1 @@
+import{S as s,i as n,s as a,ab as i,a7 as o,ac as l}from"./index.e5f15ed8.js";function c(t){return new i(o(l).oidcSettings).signinSilentCallback().catch(e=>{console.log(e)}),[]}class u extends s{constructor(e){super(),n(this,e,c,null,a,{})}}export{u as default};
